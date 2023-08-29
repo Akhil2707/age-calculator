e920d64a385d72bc93c6911572b2ee2466299d13
maneeesha 178 <akhilrathipelly07@gmail.com>
2023-08-29 23:26:47 +0530

    designed layout

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,13 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import "./calculator.css";
 
 const Calcualtor = () => {
+    const [years,setYears]=useState("");
+    const[months,setMonths]=useState("");
+    const[days,setDays]=useState("");
 
     return (
         <div className="cover">
             <h1>Age Calculator</h1>
             <input type="date" className="birthday-date" />
             <button type="submit" className="calculate-btn">calculate</button>
+            <p>your age is {years} years {months} months and  {days} days</p>
         </div>
     )
 };
